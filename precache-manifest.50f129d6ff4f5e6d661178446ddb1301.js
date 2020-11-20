ccc4efd50362513242c1d92c786f0bc0a15e700a
JB Profile <cool508@naver.com>
2020-11-20 23:32:03 +0900

    Updates

---
+++ precache-manifest.50f129d6ff4f5e6d661178446ddb1301.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120defb83660fb946a468c1702354450",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "f5eea0ce186db01bf7bd",
+    "url": "/movie_app_2020/static/css/main.44e82c37.chunk.css"
+  },
+  {
+    "revision": "e2db8dbeff0daf97e999",
+    "url": "/movie_app_2020/static/js/2.3c0d2268.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.3c0d2268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5eea0ce186db01bf7bd",
+    "url": "/movie_app_2020/static/js/main.30313609.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
